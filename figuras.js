168a544210033781c7f7e6a5e088dda6b2fc14f7
yordycuevas <yordycuevas21@gmail>
2021-12-01 20:18:36 -0500

    Actualizacion js

---
+++ figuras.js
@@ -0,0 +1,14 @@
+const ladoCuadrado = 5;
+
+console.log("Los lados del cuadrado mide:" + ladoCuadrado +" cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log("El Area del cuadrado es: " + areaCuadrado + " cm2");
+
+
+// Triangulos
